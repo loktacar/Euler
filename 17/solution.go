fbda17de51db902590961630c7356fefbc054999
Viktor Alex <viktoralex@365.is>
2014-01-16 19:09:58 +0000

    New problems, new solutions

---
+++ 17/solution.go
@@ -0,0 +1,116 @@
+package main
+
+import "fmt"
+import "math"
+
+var numbers = map[int]string{
+  1: "one",
+  2: "two",
+  3: "three",
+  4: "four",
+  5: "five",
+  6: "six",
+  7: "seven",
+  8: "eight",
+  9: "nine",
+  10: "ten",
+  20: "twenty",
+  30: "thirty",
+  40: "forty",
+  50: "fifty",
+  60: "sixty",
+  70: "seventy",
+  80: "eighty",
+  90: "ninety",
+  100: "onehundred",
+  200: "twohundred",
+  300: "threehundred",
+  400: "fourhundred",
+  500: "fivehundred",
+  600: "sixhundred",
+  700: "sevenhundred",
+  800: "eighthundred",
+  900: "ninehundred",
+  1000: "onethousand",
+}
+
+var exceptions = map[int]string{
+  11: "eleven",
+  12: "twelve",
+  13: "thirteen",
+  14: "fourteen",
+  15: "fifteen",
+  16: "sixteen",
+  17: "seventeen",
+  18: "eighteen",
+  19: "nineteen",
+}
+
+func num_to_string(number int) string {
+  exception_text, ok := exceptions[number]
+  // is an exception
+  if ok {
+    return exception_text
+  }
+
+  // one thousand
+  if number == 1000 {
+    return numbers[number]
+  }
+
+  var hundreds_text string
+
+  // hundreds
+  hundreds := math.Floor(float64(number) / 100.0)
+  if hundreds > 0 {
+    hundreds_text = numbers[int(hundreds * 100)]
+  }
+
+  number = int(float64(number) - hundreds * 100.0)
+
+  var tens_text string
+
+  // tens
+  exception_text, ok = exceptions[number]
+  if ok && hundreds_text != "" {
+    return hundreds_text + "and" + exception_text
+  } else if ok {
+    return exception_text
+  }
+
+  tens := math.Floor(float64(number) / 10.0)
+  if tens > 0 {
+    tens_text = numbers[int(tens * 10)]
+  }
+
+  number = int(float64(number) - tens * 10)
+
+  singles_text := numbers[int(number)]
+
+  if hundreds_text != "" && (tens_text != "" || singles_text != "") {
+    return hundreds_text + "and" + tens_text + singles_text
+  } else if hundreds_text != "" {
+    return hundreds_text
+  }
+
+  return tens_text + singles_text
+}
+
+func main() {
+  /*
+  thing := num_to_string(18)
+  fmt.Println(thing)
+  fmt.Println(len(thing))
+  */
+
+  var total_text string
+  num := 1
+  for num <= 1000 {
+    total_text += num_to_string(num)
+    //fmt.Println(num_to_string(num))
+    num += 1
+  }
+
+  //fmt.Println(total_text)
+  fmt.Println(len(total_text))
+}
---
+++ 24/solution.go
@@ -0,0 +1,67 @@
+package main
+
+import "fmt"
+
+func main () {
+  //list := []int{1, 2, 3, 4}
+  list := []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}
+
+  c := 1
+  for {
+    //fmt.Println(fmt.Sprintf("%d: %v", c, list))
+
+    //////
+    // Find the largest index k such that a[k] < a[k + 1]. If no such index exists, the permutation is the last permutation.
+    k := -1
+    i := 0
+    for i+1 < len(list) {
+      if list[i] < list[i+1] {
+        k = i
+      }
+      i += 1
+    }
+    if k == -1 {
+      break // last permutation
+    }
+
+    //////
+    // Find the largest index l such that a[k] < a[l].
+    l := 0
+    i = 0
+    for i < len(list) {
+      if list[k] < list[i] {
+        l = i
+      }
+      i += 1
+    }
+
+    //////
+    // Swap the value of a[k] with that of a[l].
+    old_k := list[k]
+    list[k] = list[l]
+    list[l] = old_k
+
+    //////
+    // Reverse the sequence from a[k + 1] up to and including the final element a[n].
+
+    // Copy slices
+    to_reverse := append([]int(nil), list[k+1:]...)
+    list = append([]int(nil), list[:k+1]...)
+
+    // Reverse
+    i = len(to_reverse) - 1
+    for i >= 0 {
+      list = append(list, to_reverse[i])
+      i -= 1
+    }
+
+    c += 1
+
+    // Stop on one MILLION permutations
+    if c == 1000000 {
+      break
+    }
+  }
+
+  fmt.Println(list)
+}
