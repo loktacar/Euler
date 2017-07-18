db151156fa3a7557f3b1bdbe55b6b43f6e6b9973
Viktor Alex <viktoralex@365.is>
2017-07-18 09:32:39 +0000

    Adding descriptions of the problems

--- 145/solve.py
+++ 145/solve.py
@@ -1,5 +1,13 @@
 #!/usr/bin/env python3
 
+"""
+Some positive integers n have the property that the sum [ n + reverse(n) ] consists entirely of odd (decimal) digits. For instance, 36 + 63 = 99 and 409 + 904 = 1313. We will call such numbers reversible; so 36, 63, 409, and 904 are reversible. Leading zeroes are not allowed in either n or reverse(n).
+
+There are 120 reversible numbers below one-thousand.
+
+How many reversible numbers are there below one-billion (10^9)?
+"""
+
 def reverse(num):
     rev = 0
 
